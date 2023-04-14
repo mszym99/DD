3246fd91ffe114bb50cc94126d1e3ff18b02cec3
mszym99 <mszymanski202@gmail.com>
2023-04-14 08:09:08 -0700

    test to see if this now works

--- README.md
+++
@@ -1,5 +0,0 @@
-# DD
-
-Javascript React front end, Python back end.
-Looking to experiment more with React and building websites.
-Dynamic Detailing... Powerwashing company in the PNW.
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+/*import Footer from "./components/Footer/Footer";*/
+import Homepage from "./components/Homepage/Homepage";
+import Navbar from "./components/Navbar/Navbar"; 
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
